83db6ae3704954e807fee882a7601d7076702ff5
gigatesseract <106117063@nitt.edu>
2019-07-14 22:51:39 +0530

    changed to add tshirt intp user table itself

--- src/controller/TshirtController.ts
+++ src/controller/TshirtController.ts
@@ -1,58 +1,65 @@
-import { Request, Response} from "express";
-import { getRepository } from "typeorm";
-import {User} from "../entity/User";
+import { Request, Response } from "express";
+import { getRepository, AdvancedConsoleLogger } from "typeorm";
+import { User } from "../entity/User";
 import { Tshirt } from "../entity/Tshirt";
+import { isNull } from "util";
 
-class TshirtController{
-    static register = async (req:Request, res:Response) =>{
-        let { size } = req.body;
+class TshirtController {
+  static register = async (req: Request, res: Response) => {
+    let { size } = req.body;
 
-        if(!(size)){
-            res.status(400).send("Size not received");
-        }
-
-        const UserRepository = getRepository(User);
-        let user: User;
-        try{
-            user = await UserRepository.findOneOrFail({roll_number: res.locals.jwtPayload.user.roll_number })
-        }catch(error){
-            console.log(error);
-            res.status(400).send();
-        }
-        let tshirt = new Tshirt()
-        tshirt.name = user;
-        tshirt.size = size;
-
-        const tshirtRepository = getRepository(Tshirt);
-        try{
-            await tshirtRepository.save(tshirt);
-        }catch(error){
-            res.status(400).send("Send Correct Values");
-        }
-        res.status(200).send("Registered");
-    };
-    static details = async (req:Request, res:Response) =>{
-        const UserRepository = getRepository(User);
-        let user: User;
-        try{
-            user = await UserRepository.findOneOrFail({roll_number: res.locals.jwtPayload.user.roll_number })
-        }catch(error){
-            console.log(error);
-            res.status(400).send();
-        }
-        console.log(user);
-        const tshirtRepository = getRepository(Tshirt);
-        let tshirt:Tshirt;
-        try{
-            console.log(res.locals.jwtPayload);
-            tshirt = await tshirtRepository.findOneOrFail({name: user})
-        }catch(error){
-            console.log(error);
-            res.status(400).send("Tshirt not registered");
-        }
-
-        res.status(201).json({"size":tshirt.size});
+    if (!size) {
+      console.log("sizeee");
+      res.status(400).send("Size not received");
     }
+
+    const UserRepository = getRepository(User);
+    let user: User;
+    try {
+      user = await UserRepository.findOneOrFail({
+        roll_number: res.locals.jwtPayload.user.roll_number
+      });
+    } catch (error) {
+      res.status(400).send("User does not exist");
+      return;
+    }
+    if (!isNull(user.t_shirt)) {
+      res.status(400).send("User already registered");
+      return;
+    }
+    user.t_shirt = size;
+
+    try {
+      let userRepository = getRepository(User);
+      await userRepository.save(user);
+    } catch (error) {
+      res.status(400).send("Send Correct Values. One of L,M,X or XL");
+      return;
+    }
+    res
+      .status(200)
+      .json({ message: "Successfully registered", size: user.t_shirt });
+  };
+  static details = async (req: Request, res: Response) => {
+    const UserRepository = getRepository(User);
+    let user: User;
+    try {
+      user = await UserRepository.findOneOrFail({
+        roll_number: res.locals.jwtPayload.user.roll_number
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(400).send("User does not exist");
+      return;
+    }
+    if (isNull(user.t_shirt)) {
+      res.status(400).send("Tshirt not registered");
+      return;
+    } else {
+      res.status(201).json({ size: user.t_shirt });
+      return;
+    }
+  };
 }
 
 export default TshirtController;
