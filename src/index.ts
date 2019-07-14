7b7c3addfb0e7a9f273457f21e0b488f29954b5e
gigatesseract <106117063@nitt.edu>
2019-07-14 22:52:22 +0530

    configuration + ts-node dep

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,30 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
+import { createConnection } from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as cors from "cors";
 import * as helmet from "helmet";
 import routes from "./routes";
-import {User} from "./entity/User";
-import {Books} from "./entity/Books";
-import {Tshirt} from "./entity/Tshirt";
-import {Events} from "./entity/Events";
+import { User } from "./entity/User";
+import { Books } from "./entity/Books";
+import { Tshirt } from "./entity/Tshirt";
+import { Events } from "./entity/Events";
 
-createConnection({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
-entities: [User, Books, Tshirt, Events],
-synchronize:false
-}).then(async connection => {
-
+createConnection()
+  .then(async connection => {
     // create express app
     const app = express();
-    
+
     app.use(cors());
     app.use(helmet());
 
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:false}));
+    app.use(bodyParser.urlencoded({ extended: false }));
 
     app.use("/", routes);
 
-    app.listen(3000, ()=>{
-        console.log("Server started on port 3000");
+    app.listen(3001, () => {
+      console.log("Server started on port 3001");
     });
-
-}).catch(error => console.log(error));
+  })
+  .catch(error => console.log(error));
