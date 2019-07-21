007667362ac35149c4a353a309355b84759e881c
gigatesseract <106117063@nitt.edu>
2019-07-21 18:40:30 +0530

    added bgcolor etc for events

--- src/config/config.example.ts
+++
@@ -1,5 +0,0 @@
-export default{
-    jwtSecret:"",
-    hashSalt:0,
-    expiryTime:"1h"
-}
--- src/entity/Events.ts
+++ src/entity/Events.ts
@@ -1,43 +1,44 @@
-import { Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
-export enum Department{
-    ALL = "all",
-    ARCHI = "archi",
-    CHEM = "chem",
-    CSE = "cse",
-    ECE = "ece",
-    EEE = "eee",
-    ICE = "ice",
-    PROD = "prod",
-    MECH = "mech",
-    META = "meta"
+export enum Department {
+  ALL = "all",
+  ARCHI = "archi",
+  CHEM = "chem",
+  CSE = "cse",
+  ECE = "ece",
+  EEE = "eee",
+  ICE = "ice",
+  PROD = "prod",
+  MECH = "mech",
+  META = "meta"
 }
 
 @Entity()
-export class Events{
+export class Events {
+  @PrimaryGeneratedColumn()
+  event_id: number;
 
-    @PrimaryGeneratedColumn()
-    event_id:number;
+  @Column("longtext")
+  name: string;
 
-    @Column("longtext")
-    title:string;
+  @Column("boolean")
+  allDay: boolean;
 
-    @Column("boolean")
-    allDay:boolean;
+  @Column("datetime")
+  start: Date;
 
-    @Column("datetime")
-    start:Date;
+  @Column("datetime")
+  end: Date;
 
-    @Column("datetime")
-    end:Date;
+  @Column("varchar")
+  bgColor: string;
 
-    @Column("varchar")
-    bgColor:string;
-    
-    @Column({
-        type:"enum",
-        enum:Department
-    })
-    department:Department;
-    
+  @Column("varchar")
+  description: string;
+
+  @Column({
+    type: "enum",
+    enum: Department
+  })
+  department: Department;
 }
